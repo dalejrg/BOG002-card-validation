1c185af735625ccc8c683e8ad94d9d6a491eec58
Dayana Rodriguez <dayanarodriguez120@gmail.com>
2021-02-22 15:45:49 -0500

    ocultamos la interfaz uno para poder mostrar la interfaz dos con la funcion del boton comprar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+//variables interfaz
+var btnComprar= document.getElementById('comprar')
+ btnComprar.addEventListener('click', myFunction);
+function myFunction() {
+
+    var mostInterfaz2 = document.getElementById("interfaz2");
+    var ocultInterfaz1 = document.getElementById("interfaz1");
+
+
+
+       if(mostInterfaz2.style.display == 'block'){
+           mostInterfaz2.style.display = 'none';
+           ocultInterfaz1.style.display = 'block';
+       }else{
+          mostInterfaz2.style.display = 'block';
+          ocultInterfaz1.style.display = 'none';
+         }
+   }
