065386c1fa5ed061842ce4c19b04f27966a4dd92
SMilena <sandramilenagc8@gmail.com>
2021-02-22 19:01:52 -0500

    Agregamos el diseño de la interfaz 2

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import validator from './validator.js';
 
 console.log(validator);
 
-//variables interfaz
+//Boton comprar
 var btnComprar= document.getElementById('comprar')
  btnComprar.addEventListener('click', myFunction);
-function myFunction() {
-
+function myFunction() 
+{
     var mostInterfaz2 = document.getElementById("interfaz2");
     var ocultInterfaz1 = document.getElementById("interfaz1");
-
-
 
        if(mostInterfaz2.style.display == 'block'){
            mostInterfaz2.style.display = 'none';
            ocultInterfaz1.style.display = 'block';
-       }else{
+        }
+       else{
           mostInterfaz2.style.display = 'block';
           ocultInterfaz1.style.display = 'none';
-         }
-   }
+        }
+}
